d5042b2df67b94cc762a8473b29cc93dc85c48c8
KoheiIto777 <kohei.ito@am.ics.keio.ac.jp>
2021-10-12 15:28:32 +0900

    gaが交叉まで完成

--- galib.py
+++ galib.py
@@ -1,5 +1,8 @@
 import time
 import random
+import copy
+import collections
+import sys, traceback
 
 import networkx as nx
 
@@ -10,15 +13,197 @@ from deap import creator
 
 # my library
 from allocatorunit import AllocatorUnit, App, Pair, VNode
+import oplib
+
+def mask_generator(sorted_vNode_id_list):
+    l0, l1 = dict(), dict()
+    for vNode_id in sorted_vNode_id_list:
+        bit = random.randint(0, 1)
+        l0[vNode_id] = bit
+        l1[vNode_id] = bit ^ 1
+    return l0, l1
+
+#--------------------------------------------------------------
+def cx_by_mask(parent0, parent1, mask):
+    child = copy.deepcopy(parent0)
+
+    for vNode_id, bit in mask.items():
+        vNode = child.vNode_dict[vNode_id]
+        if bit == 0:
+            # deallocate pairs whose dst or src is changed
+            for pair in vNode.send_pair_list:
+                if mask[pair.dst_vNode.vNode_id] != bit:
+                    oplib.pair_deallocation(child, pair.pair_id)
+            
+            for pair in vNode.recv_pair_list:
+                if mask[pair.src_vNode.vNode_id] != bit:
+                    oplib.pair_deallocation(child, pair.pair_id)
+        
+        elif bit == 1:
+            old_rNode_id = vNode.rNode_id
+            new_rNode_id = parent1.vNode_dict[vNode_id].rNode_id
+            # node deallocation (update the list and dict)
+            if old_rNode_id is not None:
+                child.temp_allocated_rNode_dict.pop(old_rNode_id)
+                child.empty_rNode_set.add(old_rNode_id)
+            # temporary node allocation
+            try:
+                child.empty_rNode_set.remove(new_rNode_id)
+            except KeyError:
+                # when new_rNode_id has already been secured, release new_rNode_id
+                target_vNode_id = child.temp_allocated_rNode_dict[new_rNode_id]
+                child.vNode_dict[target_vNode_id].rNode_id = None
+                #raise KeyError("vNode_id: {}, new_rNode_id: {}, empty: {}".format(vNode_id, new_rNode_id, child.empty_rNode_set))
+            child.temp_allocated_rNode_dict[new_rNode_id] = vNode.vNode_id
+            # update rNode_id
+            vNode.rNode_id = new_rNode_id
+
+            # update pairs
+            for pair in vNode.send_pair_list:
+                if mask[pair.dst_vNode.vNode_id] != bit:
+                    oplib.pair_deallocation(child, pair.pair_id)
+                else:
+                    path = parent1.pair_dict[pair.pair_id].path
+                    oplib.pair_allocation(child, pair.pair_id, path)
+            
+            for pair in vNode.recv_pair_list:
+                if mask[pair.src_vNode.vNode_id] != bit:
+                    oplib.pair_deallocation(child, pair.pair_id)
+                else:
+                    path = parent1.pair_dict[pair.pair_id].path
+                    oplib.pair_allocation(child, pair.pair_id, path)
+        
+        else:
+            old_rNode_id = vNode.rNode_id
+            vNode.rNode_id = None
+            # node deallocation (update the list and dict)
+            if old_rNode_id is not None:
+                child.temp_allocated_rNode_dict.pop(old_rNode_id)
+                child.empty_rNode_set.add(old_rNode_id)
+
+            # send-path deallocation
+            for send_pair in vNode.send_pair_list:
+                if send_pair.path is not None:
+                    oplib.pair_deallocation(child, send_pair.pair_id)
+
+            # recv-path deallocation
+            for recv_pair in vNode.recv_pair_list:
+                if recv_pair.path is not None:
+                    oplib.pair_deallocation(child, recv_pair.pair_id)
+
+    for vNode in child.allocating_vNode_list:
+        if vNode.rNode_id is None:
+            oplib.random_node_allocation(child, vNode.vNode_id)
+    
+    for pair in child.allocating_pair_list:
+        if pair.path is None:
+            oplib.random_pair_allocation(child, pair.pair_id)
+    
+    return child
+
+
+#--------------------------------------------------------------
+def cx_uniform(parent0, parent1):
+    if len(parent0.temp_allocated_rNode_dict) != len(parent1.temp_allocated_rNode_dict):
+        print("Error: The number of nodes being allocated is different for each individual.")
+        sys.exit(5)
+    
+    if len(parent0.allocating_pair_list) != len(parent1.allocating_pair_list):
+        print("Error: The number of communications being allocated is different for each individual.")
+        sys.exit(6)
+    
+    # make masks
+    sorted_vNode_id_list = sorted(parent0.temp_allocated_rNode_dict.values())
+    mask0, mask1 = mask_generator(sorted_vNode_id_list)
+
+    # for debug
+    print("\n\nbefore duplication check")
+    print("mask0: {}".format(mask0.values()))
+    print("mask1: {}\n".format(mask1.values()))
+
+    ## check the duplication
+    next_child0_rNode_dict = {vNode_id: parent1.vNode_dict[vNode_id].rNode_id if bit else parent0.vNode_dict[vNode_id].rNode_id \
+                                for vNode_id, bit in mask0.items()}
+    counter = collections.Counter(list(next_child0_rNode_dict.values()))
+    for key, count in counter.items():
+        if count == 2:
+            selected = random.randint(0, 1)
+            current = 0
+            for vNode_id, rNode_id in next_child0_rNode_dict.items():
+                if rNode_id == key and current != selected:
+                    mask0[vNode_id] = 2
+                    current += 1
+                    break
+                elif rNode_id == key and current == selected:
+                    current += 1
+    
+    next_child1_rNode_dict = {vNode_id: parent1.vNode_dict[vNode_id].rNode_id if bit else parent0.vNode_dict[vNode_id].rNode_id \
+                                for vNode_id, bit in mask1.items()}
+    counter = collections.Counter(list(next_child1_rNode_dict.values()))
+    for key, count in counter.items():
+        if count == 2:
+            selected = random.randint(0, 1)
+            current = 0
+            for vNode_id, rNode_id in next_child1_rNode_dict.items():
+                if rNode_id == key and current != selected:
+                    mask1[vNode_id] = 2
+                    current += 1
+                    break
+                elif rNode_id == key and current == selected:
+                    current += 1
+    print("next_child0_rNode_dict: {}".format(next_child0_rNode_dict.values()))
+    print("next_child1_rNode_dict: {}\n".format(next_child1_rNode_dict.values()))
+
+    print("after duplication check")
+    print("mask0: {}".format(mask0.values()))
+    print("mask1: {}\n\n".format(mask1.values()))
+
+    # exectute crossbar
+    child0 = cx_by_mask(parent0, parent1, mask0)
+    child1 = cx_by_mask(parent0, parent1, mask1)
+    return child0, child1
+
+#--------------------------------------------------------------
+class Evaluator:
+    def __init__(self):
+        self.__funcs = [
+            ('slots', lambda ind: ind.get_slot_num(), -1.0), 
+            ('hops', lambda ind: ind.get_total_communication_hops(), -1.0)
+        ]
+    
+    def eval_list():
+        return [func[0] for func in self.__funcs]
+
+    def evaluate(individual):
+        return [self.func[1](individual) for func in self.__funcs]
+
+    def weights():
+        return tuple(self.func[2] for func in self.__funcs)
+
 
 #--------------------------------------------------------------
 class GA:
     def __init__(self, seed):
         self.toolbox = base.Toolbox()
 
-        # settings to use deap
-        creator.create("Fitness", base.Fitness, weights=(TBD))
+        # create evaluation tool
+        eval_tool = Evaluator()
+
+        # instance settings
+        creator.create("Fitness", base.Fitness, weights=eval_tool.weights())
         creator.create("Individual", AllocatorUnit, fitness=creator.Fitness)
 
-        self.
+        # toolbox settings
+        self.toolbox.register("individual", creator.Individual, None, seed)
+        self.toolbox.register("population", tools.initRepeat, list, self.toolbox.Individual)
+        self.toolbox.register("evaluate", eval_tool.evaluate)
+        self.toolbox.register("mate", cx_uniform)
+        self.toolbox.register("mutate", TBD)
+        self.toolbox.register("select", tools.selTournamentDCD)
+
+        # statistics settings
+        self.stats = tools.Statistics(key=lambda ind: ind.fitness.values)
+        self.stats.register("avg", numpy.mean, axis=0)
+        self.stats.register("min", numpy.min, axis=0)
+        self.stats.register("max", numpy.max, axis=0)
 
